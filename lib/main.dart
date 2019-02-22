89cd1f8cdf92c27336957c8212453bc681573d88
kongaman <christian.kranz@gmail.com>
2019-02-22 15:21:22 +0100

    Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-      MyStatelessWidget()
+void main() => runApp(
+      MaterialApp(
+        home: MyStatefulWidget(),
+      )
   );
+
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class MyStatelessWidget extends StatelessWidget {
-
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
-
-    return Container(color: Colors.red,
-        child: Center(
-            child: Text("Hallo from Flutter", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 32.0, color: Colors.white))
-        )
+    return Container(
+      color: Colors.blueAccent,
+      child: Center(
+        child: RaisedButton(
+          child: Text(
+            "Click",
+            textDirection: TextDirection.ltr,
+          ),
+          onPressed: () {
+            print("Hi from statefullWidget");
+          },
+        ),
+      ),
     );
   }
 }
 
+//class MyStatelessWidget extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//    return Container(color: Colors.red,
+//        child: Center(
+//            child: Text("Hallo from Flutter", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 32.0, color: Colors.white))
+//        )
+//    );
+//  }
+//}
